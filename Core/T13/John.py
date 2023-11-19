96a233b8f3f61a08d8006fbd3a45521ab0101007
RobertaDobra <147082040+RobertaDobra@users.noreply.github.com>
2023-11-19 15:03:57 +0000

    Update John.py

--- Core/T13/John.py
+++ Core/T13/John.py
@@ -4,5 +4,3 @@ while name != "John":
     name_list.append(name)
     name = input("Please enter a name: ")
 print(name_list)
-
-#I cannot figure out how to eliminate case sensitivity with lower() or upper() when entering 'john' or 'John' or 'joHn' or other alternating cases within the string
